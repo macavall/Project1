4f9aa171de6371c0408aba3b24dcf840123ad07c
Matt <macavall@microsoft.com>
2024-08-10 12:21:42 -0500

    first

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Adding a note to this Project1 Program.cs file
+        Console.WriteLine("Hello, World!");        
     }
 }
